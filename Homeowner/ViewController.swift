7c6fca71492fe237380a14b9872395a7df1da55c
defaultprofile <19016906@g200010-19f.local>
2019-10-26 15:53:39 -0400

    Initial Commit

---
+++ Homeowner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homeowner
+//
+//  Created by Sukhpreet Randhawa on 2019-10-26.
+//  Copyright © 2019 Sukhpreet Randhawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
